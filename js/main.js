a3027afcc36566df813b6a1b125313e75f38ccf3
marykasp <kasparian.mary@gmail.com>
2022-05-26 10:32:23 -0500

    Display info on card for pokemon fetched

--- js/main.js
+++ js/main.js
@@ -3,8 +3,45 @@ const card = document.getElementById("card");
 const btn = document.getElementById("btn");
 const search = document.querySelector("#search-input");
 
+// Generate card
 const generateCard = (pokemon) => {
   console.log(pokemon);
+  // destructure pokemon object to get necessary properties
+  const { name, stats, sprites, types } = pokemon;
+  const [stat1, stat2, stat3, stat4, stat5] = stats;
+  // stats is an array of objects - get base_stat property from first 4 objects
+  const hp = stat1.base_stat;
+  const attack = stat2.base_stat;
+  const defense = stat3.base_stat;
+  const speed = stat5.base_stat;
+  const imgSrc = sprites.other.dream_world.front_default;
+
+  card.innerHTML = `
+    <p class="hp">
+      <span>HP</span>
+        ${hp}
+    </p>
+    <img src="${imgSrc}" alt="${name}" />
+    <h2 class="poke-name">${name}</h2>
+    <div class="types">
+      <span>Type 1</span>
+      <span>Type 2</span>
+    </div>
+    <div class="stats">
+      <div>
+        <h3>${attack}</h3>
+        <p>Attack</p>
+      </div>
+      <div>
+        <h3>${defense}</h3>
+        <p>Defense</p>
+      </div>
+      <div>
+        <h3>${speed}</h3>
+        <p>Speed</p>
+      </div>
+    </div>
+  `;
 };
 
 const getPokeData = async () => {
@@ -21,3 +58,5 @@ const getPokeData = async () => {
 
 // Add Event Listeners
 btn.addEventListener("click", getPokeData);
+// display a random pokemon card when window loads
+window.addEventListener("load", getPokeData);
