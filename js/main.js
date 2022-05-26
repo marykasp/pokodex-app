1058ca6b2a9aa9000ee7680a663d9b4f58c04baa
marykasp <kasparian.mary@gmail.com>
2022-05-26 11:03:26 -0500

    Function to generate card theme color background based on main pokemon type

--- js/main.js
+++ js/main.js
@@ -1,3 +1,21 @@
+const typeColor = {
+  bug: "#26de8a",
+  dragon: "#ffeaa7",
+  electric: "#fed330",
+  fairy: "#FF0069",
+  fighting: "#30336b",
+  fire: "#f0932b",
+  flying: "#81ecec",
+  grass: "#00b894",
+  ground: "#efb549",
+  ghost: "#a55eea",
+  ice: "#74b9ff",
+  normal: "#95afc0",
+  poison: "#6c5ce7",
+  psychic: "#a29bfe",
+  rock: "#2d3436",
+  water: "#0190ff",
+};
 const url = "https://pokeapi.co/api/v2/pokemon/";
 const card = document.getElementById("card");
 const btn = document.getElementById("btn");
@@ -9,6 +27,10 @@ const appendTypes = (types) => {
     span.innerText = item.type.name;
     document.querySelector(".types").appendChild(span);
   });
+};
+
+const styleCard = (color) => {
+  card.style.background = `radial-gradient(circle at 50% 0%, ${color} 36%, #fff 36%)`;
 };
 
 // Generate card
@@ -23,6 +45,9 @@ const generateCard = (pokemon) => {
   const defense = stat3.base_stat;
   const speed = stat5.base_stat;
   const imgSrc = sprites.other.dream_world.front_default;
+
+  // Set theme color based on pokemon type
+  const themeColor = typeColor[types[0].type.name];
 
   card.innerHTML = `
     <p class="hp">
@@ -51,6 +76,7 @@ const generateCard = (pokemon) => {
   `;
 
   appendTypes(types);
+  styleCard(themeColor);
 };
 
 const getPokeData = async () => {
