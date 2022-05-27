82dd9498aaf2e6e34b4e47199e5cb7b4d4802178
Mary Kasparian <kasparian.mary@gmail.com>
2022-05-27 07:47:14 -0500

    Code custom select dropdown menu with options for the types

---
+++ js/select.js
@@ -0,0 +1,21 @@
+// make functional select options
+const optionMenu = document.querySelector('.select-menu');
+const selectBtn = document.querySelector('.select-btn');
+const options= document.querySelectorAll('.option');
+const sBtn_text = document.querySelector('.sBtn-text');
+
+
+selectBtn.addEventListener("click", () => {
+  optionMenu.classList.toggle("active")
+})
+
+options.forEach((option) => {
+  option.addEventListener('click', () => {
+    let selectedOption = option.querySelector(".option-text").innerText;
+    sBtn_text.innerText = selectedOption
+
+    optionMenu.classList.remove('active')
+    // call function to fetch data based on type
+    console.log(selectedOption)
+  })
+})
